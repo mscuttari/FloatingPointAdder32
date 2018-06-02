1785f14e287c7b9acd8c9e19912bc655cd5848ab
nsarzimadidini <39852210+nsarzimadidini@users.noreply.github.com>
2018-06-02 12:04:08 +0200

    Extender Module and Test Bench

--- isim/TestSwapN_isim_beh.exe.sim/work/TestSwapN_isim_beh.exe_main.c
+++ isim/TestSwapN_isim_beh.exe.sim/work/TestSwapN_isim_beh.exe_main.c
@@ -25,11 +25,11 @@ int main(int argc, char **argv)
 
     xsi_register_min_prec_unit(-12);
     ieee_p_2592010699_init();
-    work_a_2227434406_3212880686_init();
-    work_a_0701045745_2372691052_init();
+    work_a_4192050451_3212880686_init();
+    work_a_1349425592_2372691052_init();
 
 
-    xsi_register_tops("work_a_0701045745_2372691052");
+    xsi_register_tops("work_a_1349425592_2372691052");
 
     IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
     xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
